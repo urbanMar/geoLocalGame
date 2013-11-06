d49a5ec4cf1db72298fafb4801280e444fde46e3
Marcin Urbanczyk <urbanczyk.mar@gmail.com>
2013-11-06 21:23:15 +0100

    upp

---
+++ GeoLocGameClient/build/source/buildConfig/debug/pl/marurb/geolocgameclient/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package pl.marurb.geolocgameclient;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
